91247bf0f591236c1cc73eea80a5620e4b6fc3dc
unknown <marxyng01@hotmail.com>
2022-08-11 00:12:21 -0500

    update to many files

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app =  express();
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
+
 app.set("view-engine", "ejs");
 
-//app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.use(bodyParser.urlencoded({extended: true}));
 //app.use(express.static(__dirname));
 
 
@@ -12,6 +17,22 @@ app.set("view-engine", "ejs");
 app.get("/", function(req, res){
 
     var today= new Date();
+
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    var day = today.toLocaleDateString("en-US", options);
+
+
+
+
+
+
+//}
+    /*
     var currentDay = today.getDay();
     var day= "";
 
@@ -40,9 +61,20 @@ app.get("/", function(req, res){
         default:
         console.log("Error: current day is equal to: " + currentDay);
   }
-        res.render("index.ejs", {kindOfDay: day } );
+  */
+        res.render("index.ejs", {kindOfDay: day, newListItems: items } );
     
 });
+
+app.post("/", function(req, res){
+   var item= req.body.newItem;
+
+   items.push(item);
+  
+   res.redirect("/");
+});
+
+//res.render("index.ejs", {newListItem: item});
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
