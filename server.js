0942592fc73f740760a45bd937aab2f54d356032
unknown <marxyng01@hotmail.com>
2022-08-14 18:46:40 -0500

    update with new file and server

--- server.js
+++ server.js
@@ -76,6 +76,6 @@ app.post("/", function(req, res){
 
 //res.render("index.ejs", {newListItem: item});
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server started on port 3000");
 });
